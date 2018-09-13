c806d4ced319d0002379361890333384837e6043
APCS2nd <APCS2nd@MHS-Dreyer-01>
2018-09-13 09:50:27 -0700

    Expanded Calculate.java (methods library)

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -1,11 +1,13 @@
-/* This is the client code for the Calculate Library.
+/* This calls various methods in the Calculate Library.
+ * Client Code
  * @author Bryan Chan
  * @version 0 September 6, 2018
  */
 public class DoMath {
 
 	public static void main(String[] args) {
-		
+		System.out.println(Calculate.square(-3));
+		System.out.println(Calculate.toImproperFrac(3,1,2));
+		System.out.println(Calculate.toMixedNum(7,2));
 	}
-
 }
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -5,7 +5,43 @@
 
 public class Calculate {
 	
-	public int square(int number) {
-		return number * number;		
+	//returns square of integer
+	public static int square(int number) {
+		return number * number;
 	}
-}
+	//returns cube of integer
+	public static int cube(int number) {
+		return number * number * number;
+	}
+	//returns average of two doubles
+	public static double average(double number1, double number2) {
+		return (number1 + number2)/2;
+	}
+	//returns average of three doubles
+	public static double average(double number1, double number2, double number3) {
+		return(number1 + number2 + number3)/3;
+	}
+	//returns a conversion of radians to degrees
+	public static double toDegrees(double number) {
+		double pi = 3.14159; //Pi approximation
+		return (number/pi) * 180;
+	}
+	//returns a conversion of degrees to radians
+	public static double toRadians(double number) {
+		double pi = 3.14159; //Pi approximation
+		return(number/180) * pi;
+	}
+	//returns the discriminant  of a quadratic equation in standard form
+	public static double discriminant(double a, double b, double c) {
+		return b*b-(4*a*c);
+	}
+	//returns the conversion of a mixed number to an improper fraction
+	public static String toImproperFrac(int wholeNumber, int numerator, int denominator) {
+		return ((wholeNumber*denominator+numerator) + "/" + (denominator));
+	}
+	//returns the conversion of a improper fraction to a mixed number
+	public static String toMixedNum(int numerator, int denominator) {
+		return((numerator/denominator) + "_" + (numerator%denominator) + "/" + denominator);
+	}
+	//
+}	
