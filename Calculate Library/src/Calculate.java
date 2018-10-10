8ce8c0217ec44beddd2f667c690978c125597981
bchan <bchan@LAPTOP-79T7FS0N>
2018-10-09 22:42:46 -0700

    Update Version 1

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -1,6 +1,6 @@
 /* This class contains the methods to do various math related task.
  * @author Bryan Chan
- * @version 0 September 6, 2018
+ * @version 1 October 9, 2018
  */
 
 public class Calculate {
